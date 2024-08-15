5c4e15e803fe4ea1939b0122a5d6dee343897286
Matthew Burke <47010776+mattburke1999@users.noreply.github.com>
2024-08-14 20:40:27 -0500

    moved app outside of main

---
+++ src/App/App.jsx
@@ -0,0 +1,36 @@
+
+import HelloPage from '../HomePage/Hello.jsx'
+import NavBar from '../NavBar/NavBar.jsx'
+import Stack from '../Stack/Stack.jsx'
+import Experience from '../Experience/Experience.jsx'
+import { useRef } from 'react';
+import styles from './main.module.css';
+import Background from '../Background/Background.jsx'
+
+export default function App() {
+    const homeRef = useRef(null);
+    const stackRef = useRef(null);
+    const expRef = useRef(null);
+    // const ProjectsRef = useRef(null);
+    const topPageBreakRef = useRef(null);
+    const bottomPageBreakRef = useRef(null);
+  
+    const scrollToSection = (sectionRef) => {
+      window.scrollTo({
+        top: sectionRef.current.offsetTop,
+        behavior: 'smooth', // Smooth scroll
+      });
+    };
+    return (
+        <>
+            <Background className={styles.bg}/>
+            <NavBar scrollToSection={scrollToSection} homeRef={homeRef} stackRef={stackRef} expRef={expRef}/>
+            <HelloPage homeRef={homeRef}/>
+            <hr className={styles.pageBreak} ref={topPageBreakRef}/>
+            <Stack stackRef={stackRef} bottomPageBreak={bottomPageBreakRef} topPageBreak={topPageBreakRef}/>
+            <hr className={styles.pageBreak} ref={bottomPageBreakRef}/>
+            <Experience expRef={expRef}/>
+
+        </>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HelloPage from './HomePage/Hello.jsx'
-import NavBar from './NavBar/NavBar.jsx'
-import Stack from './Stack/Stack.jsx'
-import Experience from './Experience/Experience.jsx'
-import { useRef } from 'react';
-import styles from './main.module.css';
-import Background from './Background/Background.jsx'
+import App from './App/App'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
@@ -14,30 +9,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </React.StrictMode>,
 );
 
-
-function App() {
-    const homeRef = useRef(null);
-    const stackRef = useRef(null);
-    const expRef = useRef(null);
-    // const ProjectsRef = useRef(null);
-    const pageBreakRef = useRef(null);
-  
-    const scrollToSection = (sectionRef) => {
-      window.scrollTo({
-        top: sectionRef.current.offsetTop,
-        behavior: 'smooth', // Smooth scroll
-      });
-    };
-    return (
-        <>
-            <Background className={styles.bg}/>
-            <NavBar scrollToSection={scrollToSection} homeRef={homeRef} stackRef={stackRef} expRef={expRef}/>
-            <HelloPage homeRef={homeRef}/>
-            <hr className={styles.pageBreak}/>
-            <Stack stackRef={stackRef} pageBreak={pageBreakRef}/>
-            <hr className={styles.pageBreak} ref={pageBreakRef}/>
-            <Experience expRef={expRef}/>
-
-        </>
-    );
-}
