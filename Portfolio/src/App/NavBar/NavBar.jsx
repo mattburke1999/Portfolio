d7baafc4739f1a055d5763beea94c5ad75e7a16f
Matthew Burke <47010776+mattburke1999@users.noreply.github.com>
2025-06-06 00:24:41 -0500

    added logo to navbar

--- Portfolio/src/App/NavBar/NavBar.jsx
+++ Portfolio/src/App/NavBar/NavBar.jsx
@@ -3,10 +3,16 @@ import styles from './NavBar.module.css'
 export default function NavBar({homeRef, stackRef, expRef, projRef, scrollToSection, currentSection}) {
     return (
         <nav className={styles.navbar}>
-            <button className={currentSection === 'home' ? `${styles.blue} ${styles.active}` : styles.blue} onClick={() => scrollToSection(homeRef, 'home')}>Home</button> 
-            <button className={currentSection === 'stack' ? `${styles.green} ${styles.active}` : styles.green} onClick={() => scrollToSection(stackRef, 'stack')}>Tech Stack</button>
-            <button className={currentSection === 'exp' ? `${styles.pink} ${styles.active}` : styles.pink} onClick={() => scrollToSection(expRef, 'exp')}>Experience</button>
-            <button className={currentSection === 'proj' ? `${styles.blue} ${styles.active}` : styles.blue} onClick={() => scrollToSection(projRef, 'proj')}>Projects</button>
+            <div className={styles.navLogo}>
+                <img src='/name-logo.png' alt='Name Logo'/>
+                <h1>Matthew Burke Dev</h1>
+            </div>
+            <div className={styles.navBtns}>
+                <button className={currentSection === 'home' ? `${styles.blue} ${styles.active}` : styles.blue} onClick={() => scrollToSection(homeRef, 'home')}>Home</button> 
+                <button className={currentSection === 'stack' ? `${styles.green} ${styles.active}` : styles.green} onClick={() => scrollToSection(stackRef, 'stack')}>Tech Stack</button>
+                <button className={currentSection === 'exp' ? `${styles.pink} ${styles.active}` : styles.pink} onClick={() => scrollToSection(expRef, 'exp')}>Experience</button>
+                <button className={currentSection === 'proj' ? `${styles.blue} ${styles.active}` : styles.blue} onClick={() => scrollToSection(projRef, 'proj')}>Projects</button>
+            </div>
         </nav>
     );
 }
