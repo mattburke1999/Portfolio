c20ffa5cc122b5341de842675d288f70fc5b0ff9
Matthew Burke <47010776+mattburke1999@users.noreply.github.com>
2025-06-07 14:28:43 -0500

    fixing background height issues and the squares not rendering all the way down

--- Portfolio/src/App/Background/Background.jsx
+++ Portfolio/src/App/Background/Background.jsx
@@ -5,18 +5,17 @@ export default function Background() {
     const squaresRef = useRef([]);
     const allSquares = useRef([]);
 
-    const backgroundHeight = 643;
-    const backgroundPixels = window.innerHeight * backgroundHeight / 100;
-
     const numSquares = 125;
     useEffect(() => {
-        
+        const isMobile = window.innerWidth <= 768;
+        const backgroundPixels = document.getElementById('root').getBoundingClientRect().height * (isMobile ? 1.1 : 1.05);
+        console.log('Background height:', backgroundPixels); 
         const totalSquares = numSquares * 3;
 
         // Initialize square data
         allSquares.current = Array.from({ length: totalSquares }, () => ({
             x: Math.random() * window.innerWidth,
-            y: Math.random() * backgroundPixels + window.innerHeight / 2,
+            y: Math.random() * backgroundPixels,
             speed: 0.1 + Math.random() * 0.05,
             angle: Math.random() * 2 * Math.PI,
         }));
@@ -30,8 +29,8 @@ export default function Background() {
                 if (s.x > window.innerWidth) s.x = 0;
                 else if (s.x < 0) s.x = window.innerWidth;
 
-                if (s.y > backgroundPixels) s.y = window.innerHeight + 55;
-                else if (s.y < window.innerHeight + 55) s.y = backgroundPixels;
+                if (s.y > backgroundPixels) s.y = window.innerHeight;
+                else if (s.y < window.innerHeight) s.y = backgroundPixels;
 
                 const el = squaresRef.current[i];
                 if (el) {
@@ -64,7 +63,7 @@ export default function Background() {
     }
 
     return (
-        <svg className={styles.bg} width="100%" height="100%" style={{ height: `${backgroundHeight}vh` }}>
+        <svg className={styles.bg} width="100%" height="100%">
             <g id="bg-layer">
                 <rect
                     style={{
