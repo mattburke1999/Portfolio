24b3b20784df9017a7d10628bd05733368852a01
Matthew Burke <47010776+mattburke1999@users.noreply.github.com>
2026-07-05 21:55:12 -0500

    Updates

---
+++ games/service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "games-v1";
+
+const PRECACHE_URLS = [
+  "games/images/jumper.png",
+  "games/images/solitaire.png",
+  "games/images/sudoku.png",
+  "games/index.html",
+  "games/jumper/assets/index-BVxn2pB7.js",
+  "games/jumper/assets/index-DX7L0S4t.css",
+  "games/jumper/assets/jumper_wasm_bg-DFU07g8j.wasm",
+  "games/jumper/index.html",
+  "games/solitaire/assets/index-CxK_DShg.js",
+  "games/solitaire/assets/index-fLNwAP4M.css",
+  "games/solitaire/index.html",
+  "games/styles.css",
+  "games/sudoku/assets/index-BEW3mIi6.js",
+  "games/sudoku/assets/index-BR_ZM9eD.css",
+  "games/sudoku/index.html"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS))
+  );
+});
